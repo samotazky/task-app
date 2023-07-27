618093ef49b6578a31bc30a688137955cbe6514d
samotazky <samotazky82@gmail.com>
2023-07-27 18:38:58 +0200

    30. vysvetlenie funkcie map()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  // čisty Javascript - začiatok
+  // const vychodziPole = [5, 6, 7, 8, 9]
+
+  // const vyslednePole = vychodziPole.map( (jednoCislo) => {
+  //   return jednoCislo + 10
+  // })
+  
+  // console.log(vyslednePole)
+
+  const data = [
+    { id: 1, name: 'Vynést koš' },
+    { id: 2, name: 'Uklidit kuchyň' },
+    { id: 3, name: 'Zajít do fitka' },
+    { id: 4, name: 'Nakoupit' },
+    { id: 5, name: 'Nakrmit kočku' },
+    ]
+
+    const vysledok = data.map( (oneDataObject) => {
+      return oneDataObject.name
+    })
+
+    console.log(vysledok);
+  
+  // čisty Javascript - koniec
+  return(
+    <h1>Test</h1>
+  )
 }
 
-export default App;
+export default App
