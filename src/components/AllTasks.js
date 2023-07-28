12559a1f2e71d5c0111fda745353c130461d6e27
samotazky <samotazky82@gmail.com>
2023-07-28 16:06:27 +0200

    33. key (identifikátor) a pridanie button

--- src/components/AllTasks.js
+++ src/components/AllTasks.js
@@ -2,13 +2,20 @@ import "./AllTasks.css"
 import data from "../data"
 
 const AllTasks = () => {
+
+    const tasksHandler = () => {
+        console.log("klik")
+    }
+
+
     return <div>
             {
                 data.map( (oneTask) => {
                     const {id, name} = oneTask
 
-                    return <div className= "oneTask">
+                    return <div className= "oneTask" key={id}>
                         <h4>{name}</h4>
+                        <button onClick={tasksHandler}>Vymazať</button>
                     </div>
                 })
             }
